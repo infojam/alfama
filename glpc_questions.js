c2139dccee1a4bbd1ee48f308353a49f1ebe4c38
infojamjames <87974094+infojamjames@users.noreply.github.com>
2022-09-07 10:29:47 +0100

    Update glpc_questions.js

--- glpc_questions.js
+++ glpc_questions.js
@@ -1,24 +1,141 @@
 let questions = [
     {
-        id:"5466a712",
-        section_id: 'Supervision & Management',
+        id:"8f99ee50-ef40-4e76-8ab7-4261927d0e25",
+        title: 'Creativity & Innovation',
+        title_short: "C&I",
         questions:[
-            {   type:"dropdown",
-                text: 'Supervision & Management',
+            {   id:"d071f920-df37-4786-be44-8d53d59ef661",
+                order:1,
+                type:"dropdown",
+                text: 'Creativity & Innovation',
+                open: false,
                 options:[
                     {
-                        text:"Dispersal of staff",
+                        order:1,
+                        text:"1",
                         value:1,
-                        score:1,
-                        description:"Consideration should be given in all cases to whether full accountability exists, the inherent difficulties to the work being supervised/managed, and the level of difficulty of supervision/management.  In particular, where supervision/management is made more difficult because of the wide physical dispersal or mobility of staff, an additional 6 points shall be added to the factor; these additional points can only be applied up to and including Level 5.",
-                        selected:false}
+                        score:28,
+                        description:"Work with very limited opportunity for creative or innovatory thinking",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"2",
+                        value:2,
+                        score:40,
+                        description:"Work largely regulated by laid down procedures, but needing occasional creative skills to deal with routine problems.",
+                        selected:false
+                    },
+                    {
+                        order:3,
+                        text:"3",
+                        value:3,
+                        score:52,
+                        description:"Creativity is a feature of the job but exercised within the general framework of recognised procedures. ",
+                        selected:false
+                    },
+                    {
+                        order:4,
+                        text:"4",
+                        value:4,
+                        score:64,
+                        description:"Creativity and innovation are essential to the job and need to be regularly exercised within general guidelines. ",
+                        selected:false
+                    },
+                    {
+                        order:5,
+                        text:"5",
+                        value:5,
+                        score:76,
+                        description:"Work which requires a range of imaginative solutions or responses and involves application of fresh and innovatory thinking. ",
+                        selected:false
+                    },
+                    {
+                        order:6,
+                        text:"6",
+                        value:6,
+                        score:88,
+                        description:"Work which requires creative and innovative input in a number of diverse subjects and range of expertise where the opportunity and need for imaginative thinking is not limited by defined policies",
+                        selected:false
+                    },
+                    {
+                        order:7,
+                        text:"7",
+                        value:7,
+                        score:100,
+                        description:"Work carried out in new and challenging situations frequently involving innovatory response on diverse subjects which have extensive policy or service implications.",
+                        selected:false
+                    }
                 ]
-            }
+            },
+            
         ],
-        title: 'Supervision & Management',
-        
-        options: [{ helptext: '', display: '', value: '', datatype: 'dropdown' }],
-        open : true,
-        score : 0
+
+    },
+    {
+        id:"guid",
+        title: 'Contacts & Relationships',
+        title_short: "C&R",
+        questions:[
+            {   id:"guid",
+                order:1,
+                type:"dropdown",
+                text: 'Contacts & Relationships',
+                open: false,
+                options:[
+                    {
+                        order:2,
+                        text:"1",
+                        value:1,
+                        score:20,
+                        description:"Routine or incidental contacts involving exchange of information on non-contentious matters.",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"2",
+                        value:2,
+                        score:38,
+                        description:"Routine or incidental contacts involving exchange of information on non-contentious matters.",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"3",
+                        value:3,
+                        score:56,
+                        description:"Issues generally not contentious, but where the outcome may not be straight-forward. Within the Organisation, the advice or guidance would relate to issues which are less well established. Alternatively outside contacts would involve identifying details of service needs, assessment and initiating action to provide assistance, offering straightforward advice or delivering more comprehensive support and/or care.",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"4",
+                        value:4,
+                        score:74,
+                        description:"Situations where the content and outcome are not straightforward or well established and could involve more detailed assessment, planning, evaluation, care and assistance. Some authority in the provision of services is required.",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"5",
+                        value:5,
+                        score:92,
+                        description:"Some matters are likely to be contentious or complex requiring support, tact, persuasion and sensitivity, within the application of operational guidelines. The outcome will have a material effect, including care, on the person, service or organisation contacted.",
+                        selected:false
+                    },
+                    {
+                        order:2,
+                        text:"6",
+                        value:6,
+                        score:92,
+                        description:"Some matters are likely to be contentious or complex requiring support, tact, persuasion and sensitivity, within the application of operational guidelines. The outcome will have a material effect, including care, on the person, service or organisation contacted.",
+                        selected:false
+                    },
+                ]
+            },
+            
+        ],
+
     }
+
 ]
