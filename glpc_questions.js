133e17204c711a2655f23d9c7c266f38f49c89ec
infojam <andy@infojam.co.uk>
2022-09-07 09:25:05 +0100

    Create glpc_questions.js

---
+++ glpc_questions.js
@@ -0,0 +1,24 @@
+let questions = [
+    {
+        id:"5466a712",
+        section_id: 'Supervision & Management',
+        questions:[
+            {   type:"dropdown",
+                text: 'Supervision & Management',
+                options:[
+                    {
+                        text:"Dispersal of staff",
+                        value:1,
+                        score:1,
+                        description:"Consideration should be given in all cases to whether full accountability exists, the inherent difficulties to the work being supervised/managed, and the level of difficulty of supervision/management.  In particular, where supervision/management is made more difficult because of the wide physical dispersal or mobility of staff, an additional 6 points shall be added to the factor; these additional points can only be applied up to and including Level 5.",
+                        selected:false}
+                ]
+            }
+        ],
+        title: 'Supervision & Management',
+        
+        options: [{ helptext: '', display: '', value: '', datatype: 'dropdown' }],
+        open : true,
+        score : 0
+    }
+]
